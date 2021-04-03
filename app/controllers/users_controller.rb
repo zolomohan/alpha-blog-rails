a22c7235e2ec34ec7d399fd6f2ea664cc8bccfa7
zolomohan <mohanzolomon@gmail.com>
2021-04-03 17:36:27 +0530

    Add User Show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:edit, :update]
+    before_action :set_user, only: [:edit, :update, :show]
 
     def new
         @user = User.new
     end
 
     def edit
+        
+    end
+
+    def show
         
     end
 
