987d79c64e6b49f1a5084ad765d342d22fb7f6da
zolomohan <mohanzolomon@gmail.com>
2021-04-04 00:34:31 +0530

    Paginate Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,11 +4,12 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.all.paginate(page: params[:page], per_page: 10)
   end
 
   # GET /categories/1 or /categories/1.json
   def show
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 10)
   end
 
   # GET /categories/new
