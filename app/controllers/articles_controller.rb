25a6f89a09ef2ce8a2744b1a9b45f70d9fef5cd7
zolomohan <mohanzolomon@gmail.com>
2021-04-03 20:15:15 +0530

    Fix New Article User ID Vulnerability

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-
+        @article.user = current_user
         if @article.save
             flash[:notice] = "Article Created Successfully"
             redirect_to article_path(@article)
