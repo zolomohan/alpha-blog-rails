97c08224434e873cfb127085868ac1b368eff3f4
zolomohan <mohanzolomon@gmail.com>
2021-04-03 17:08:53 +0530

    Add Signup from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
+  resources :users, except: [:new]
+  get "signup", to: "users#new"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
