2fbe0fe3bdb815b8a0b2584c06809ab15e4317b7
zolomohan <mohanzolomon@gmail.com>
2021-04-03 16:49:04 +0530

    Add Password to User Schema

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'bootstrap-sass', '~> 3.4.1'
 gem 'jquery-rails'
+gem 'bcrypt', '~> 3.1', '>= 3.1.12'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 8, maximum: 25} 
     VALID_EMAIL_REGEX = /\A[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}\z/i
     validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
+
+    has_secure_password
 end
