071e6c1bc2120a1ba5631dc13b770ff08f593764
zolomohan <mohanzolomon@gmail.com>
2021-04-03 19:14:26 +0530

    Add Home Page & Redirect if Logged in

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, except: [:new]
 
+  get "/", to: "pages#home"
   get "signup", to: "users#new"
   get "login", to: "sessions#new" 
   post "login", to: "sessions#create"
