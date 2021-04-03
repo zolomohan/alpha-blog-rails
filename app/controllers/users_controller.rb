68fbfa6a948cf70a973decc11f9ff744e60913f2
zolomohan <mohanzolomon@gmail.com>
2021-04-03 20:14:01 +0530

    Restrict User Edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
+    before_action :logged_in?, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 10)
@@ -43,5 +45,12 @@ class UsersController < ApplicationController
 
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:danger] = "You can only edit your own profile"
+            redirect_to users_path
+        end
     end
 end
